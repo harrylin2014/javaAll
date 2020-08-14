a8ca8acd69966ade19319628a7c1171637084da2
linxuelei <linxuelei@haier.com>
2020-08-14 09:41:18 +0800

    【bootmybatis】first

---
+++ bootmybatis/src/test/java/com/example/bootmybatis/BootmybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootmybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- javaTest2/src/main/java/org/example/TestZS.java
+++ javaTest2/src/main/java/org/example/TestZS.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import cn.hutool.core.date.DateUtil;
+
 /**
  * @author lxl
  * @date 2020/8/4 11:36
  */
 public class TestZS {
+    public static void main(String args[]){
+
+
+
+    }
 }
