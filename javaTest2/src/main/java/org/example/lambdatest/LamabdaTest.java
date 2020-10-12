547b51ee96b6b4607cb9a116a2dfb4647d2364fd
linxuelei <linxuelei@haier.com>
2020-10-12 10:18:38 +0800

    netty实现http服务器

---
+++ javaTest2/src/main/java/org/example/lambdatest/LamabdaTest.java
@@ -0,0 +1,11 @@
+package org.example.lambdatest;
+
+/**
+ * @author lxl
+ * @date 2020/10/10 14:43
+ */
+public class LamabdaTest {
+    public static void main(String[] args) {
+        //test("1","2");
+    }
+}
---
+++ nettytest/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception {
+        HttpServer server = new HttpServer(8081);// 8081为启动端口
+        server.start();
+    }
+}
